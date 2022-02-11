f6f667f25cc7188664489940b6a3ab3dd759ed14
Sun Dong <truepai415@gmail.com>
2022-02-12 02:00:20 +0800

    change router address

--- src/config/constants/index.ts
+++ src/config/constants/index.ts
@@ -1,7 +1,7 @@
 import { ChainId, JSBI, Percent, Token } from '@pancakeswap/sdk'
 import { mainnetTokens, testnetTokens } from './tokens'
 
-export const ROUTER_ADDRESS = '0x10ED43C718714eb63d5aA57B78B54704E256024E'
+export const ROUTER_ADDRESS = '0xe92d94fc7635d062b6504b5AB4149e20e9606419'
 
 // a list of tokens by chain
 type ChainTokenList = {

5a38b43071d972b10d0f8afc722e643604d4bfa6
Sun Dong <truepai415@gmail.com>
2022-02-13 20:09:23 +0800

    change lp addresses

--- src/config/constants/farms.ts
+++ src/config/constants/farms.ts
@@ -5,7 +5,7 @@ const serializedTokens = serializeTokens()
 
 const farms: SerializedFarmConfig[] = [
   /**
-   * These 3 farms (PID 0, 251, 252) should always be at the top of the file.
+   * These 3 farms (PID 0, 1, 2) should always be at the top of the file.
    */
   {
     pid: 0,
@@ -18,21 +18,20 @@ const farms: SerializedFarmConfig[] = [
     quoteToken: serializedTokens.wbnb,
   },
   {
-    pid: 251,
     lpSymbol: '$MINTS-BNB LP',
     lpAddresses: {
       97: '0x3ed8936cAFDF85cfDBa29Fbe5940A5b0524824F4',
-      56: '0x0eD7e52944161450477ee417DE9Cd3a859b14fD0',
+      56: '0x3c33FfD756578A2C01e56A00ED681D801d891D57',
     },
     token: serializedTokens.cake,
     quoteToken: serializedTokens.wbnb,
   },
   {
-    pid: 252,
+    pid: 2,
     lpSymbol: 'BUSD-BNB LP',
     lpAddresses: {
       97: '',
-      56: '0x58F876857a02D6762E0101bb5C46A8c1ED44Dc16',
+      56: '0x66AE42917e8C08AD53E6f95d1263b4910b9E16eA',
     },
     token: serializedTokens.busd,
     quoteToken: serializedTokens.wbnb,
